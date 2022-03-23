84d536e4825fc9a544dad237e4dee1e8c0b42ae8
Tamim Hashimi <tamim.h2@gso.schule.koeln>
2022-03-23 12:50:43 +0100

    v 1

--- Methoden1/Program.cs
+++ Methoden1/Program.cs
@@ -6,7 +6,37 @@ namespace Methoden1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Geben Sie an wie oft der Name ausgegeben werden soll.");
+            int durchlauf = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben Sie den Text ein, der ausgegeben werden soll.");
+
+            string Text = Console.ReadLine();
+            int anzahl = 0;
+            anzahl = ausgabeTextMalX(durchlauf, Text);
+
+
+
+            Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+
+
+
+        }
+
+
+
+        static int ausgabeTextMalX(int durchlauf, string text)
+        {
+
+
+
+            for (int i = 0; i < durchlauf; i++)
+            {
+                Console.WriteLine(text);
+            }
+
+
+
+            return text.Length * durchlauf;
 
 
 
