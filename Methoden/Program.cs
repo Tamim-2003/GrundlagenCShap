ed40f5bec431c3681bfcc0e9816503b71ef954f9
Tamim Hashimi <tamim.h2@gso.schule.koeln>
2022-03-30 12:54:14 +0200

    Metode geschrieben

---
+++ Methoden/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Methoden
+{
+    class Program
+    {
+        private static short übernachtung;
+
+        static void Main(string[] args)
+        {
+            short[] zimmernummer = { 45, 23, 3, 32 };
+            string[] vorname = { "Jahn,Paul,George,Ringo" };
+            string  = nachname { "Lennon,McCartney,Harrison,Starr"};
+            short = übernachtung {"1,4,12,3"};
+            
+
+            Console.WriteLine("Bitte Geben Sie  meine Daten ein");
+            Console.ReadLine();
+
+            string eingabe_str = Console.WriteLine();
+            short eingabe_sh;
+            Console.ReadKey();
+
+                                        //eingabe_str, out eingabe_sh
+            bool success = short.TryParse(//eingabe_str, out eingabe_sh);
+            if (success)
+            {
+                Console.WriteLine($"Converted '{value}' to {number}.");
+            }
+            else
+            {
+                Console.WriteLine($"Attempted conversion of '{value ?? "<null>"}' failed.");
+
+            }
+    
+}
+    }
